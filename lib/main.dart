6aa38dd881ed97c8bd4798952dd7a160c6be9b59
katoemma <emmanuelkato39@gmail.com>
2024-01-10 15:27:43 +0300

    completed the MiCard challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
--import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(myApp());
@@ -11,27 +11,66 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.green[900],
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Icon(Icons.menu,size: 40,color: Colors.green[900],),
+          title: Text('KIJANI APP', style: TextStyle(fontWeight: FontWeight.bold),),
+        ),
         body: SafeArea(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-               const CircleAvatar(
-                  radius: 60.0,
-                  foregroundImage: NetworkImage('https://images.unsplash.com/photo-1531384441138-2736e62e0919?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBtYW58ZW58MHx8MHx8fDA%3D'),
-               ),
-                //users names
-                Container(
-                  padding: EdgeInsets.all(5.0),
-                  child: Text('Kato Emma', style: TextStyle(color: Colors.white,fontSize: 35,fontFamily: 'Pacifico'),textAlign: TextAlign.center,),
-                )
-              ],
-            ),
-          )
-        )
-    )
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50,
+                foregroundImage: NetworkImage(
+                    'https://media.istockphoto.com/id/1269495342/photo/portrait-of-black-african-young-man-smiling-while-walking-in-road-of-kilembe-town-uganda.jpg?s=612x612&w=0&k=20&c=jsTihqtyXDyvwQLX-Oze2dioAlFyf6z_ZRCSzgukBYg='),
+              ),
+              const Text(
+                'Kato Emmanuel',
+                style: TextStyle(
+                    color: Colors.white, fontSize: 40, fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.green[300],
+                    fontSize: 25,
+                    fontFamily: 'Barlow Condensed'),
+              ),
+              const SizedBox(
+                width: 150,
+                child: Divider(
+                  color: Colors.tealAccent,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.green[500],
+                  ),
+                  title: Text(
+                    '+256778707801',
+                    style: TextStyle(
+                        color: Colors.green[500],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.mail,color: Colors.green[500],),
+                  title: Text('kato@email.com', style: TextStyle(color: Colors.green[500], fontSize: 20,fontWeight: FontWeight.bold),),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
